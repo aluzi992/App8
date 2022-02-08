eaa80d531882c4dcf09d2795903b4d9f5f259c40
liubin <f5liubin@hotmail.com>
2022-02-08 11:11:24 +0800

    添加项目文件。

---
+++ App8/App8/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using App8.ViewModels;
+using App8.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace App8
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ App8/App8/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using App8.Models;
+using App8.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App8.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
